c7511f58cdeacf4c8a45cbd05ec37d13939c8988
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-05-03 13:49:57 +0100

    initialized the index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+const Home = () => {
+  return (
+    <>
+      {/* herobanner component */}
+      HeroBanner
+      <div>
+        <h2>Best Selling product</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      {/* product compoennt */}
+      <div>
+        {["product 1", "product 2", "product 3"].map((product) => product)}
+      </div>
+      {/* Footer component */}
+      Footer
+    </>
+  );
 };
 
-export default index;
+export default Home;
