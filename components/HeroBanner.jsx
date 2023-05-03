b02edb79d1aa8e1e31b9a9d91921cb9a30a83ea0
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-05-03 14:31:23 +0100

    done with the file and folder structure

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Herobanner = () => {
+  return <div>Herobanner</div>;
+};
+
+export default Herobanner;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 
+import { Product, FooterBanner, Herobanner } from "@/components";
+
 const Home = () => {
   return (
     <>
       {/* herobanner component */}
-      HeroBanner
-      <div>
+      <Herobanner />
+      <div className='products-heading'>
         <h2>Best Selling product</h2>
         <p>Speakers of many variations</p>
       </div>
       {/* product compoennt */}
-      <div>
+      <div className='products-container'>
         {["product 1", "product 2", "product 3"].map((product) => product)}
       </div>
       {/* Footer component */}
