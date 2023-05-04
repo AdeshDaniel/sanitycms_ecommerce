9cf864ff756dacf29a3b22f6c1487f9079ed8fae
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-05-04 19:42:11 +0100

    done with the footerbanner

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ const Home = ({ products, bannerData }) => {
         ))}
       </div>
       {/* Footer component */}
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
 };
